231e14da70625f27ebfb37011e7552e0595c2068
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2025-07-03 21:34:55 +0530

    rttr

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   darkMode: "class", // <== important for dark mode
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: "16px", // ensure default font size is 16px
+      },
+    },
   },
   plugins: [],
 };
