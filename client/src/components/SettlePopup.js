1aa0a6752752c091efb50abf5d92e1f6a0d77ce5
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2025-07-04 07:35:35 +0530

    handle settle

--- client/src/components/SettlePopup.js
+++ client/src/components/SettlePopup.js
@@ -99,36 +99,13 @@ const SettlePopup = ({ handleCloseSettlePopup }) => {
     });
   };
 
-  const getRecordId = async (invoicenumber) => {
-    const invoiceInfo_CollectionRef = collection(db, "Invoice_Info");
-    const data = await getDocs(invoiceInfo_CollectionRef);
-    const filteredData = data.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
-
-    if (!filteredData) return;
-
-    const loggedInUser = localStorage.getItem("user");
-
-    const allBrandsInfo = filteredData.filter(
-      (x) =>
-        x.loggedInUser === loggedInUser &&
-        x.invoiceInfo.invoiceNumber === invoicenumber
-    );
-
-    setDocId(allBrandsInfo[0].id);
-  };
-
   useEffect(() => {
     const settleInfo = JSON.parse(localStorage.getItem("settleInfo"));
     setAmount(settleInfo.amount);
     setBalance(settleInfo.balance);
     setAdvance(settleInfo.advance);
-
     setInvoiceNumber(settleInfo.invoicenumber);
-
-    getRecordId(settleInfo.invoicenumber);
+    setDocId(settleInfo.docid);
   }, []);
 
   return (
